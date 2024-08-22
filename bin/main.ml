2b8d647cc7542df51a2f95172f8dd2fbd6aa87f1
Landry Geiger <69441011+landrygeiger@users.noreply.github.com>
2024-08-22 10:08:58 -0500

    Change messages code to exist in submodule

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,7 @@ let () =
   Dotenv.export ();
   let api_token = Sys.getenv "GROUPME_API_TOKEN" in
   let messages_request =
-    Haikus.Groupme.fetch_messages_for_group ~api_token ~group_id:"90085950"
+    Haikus.Groupme.Message.fetch_all_for_group ~api_token ~group_id:"90085950"
       ~chunk_size:100
   in
   Lwt_main.run messages_request |> List.length |> Int.to_string |> print_endline
--- lib/groupme.mli
+++ lib/groupme.mli
@@ -2,11 +2,14 @@ module Group : sig
   type t = { id : string; name : string }
 
   val from_json : Yojson.Basic.t -> t
-  val from_body : Cohttp_lwt.Body.t -> t list Lwt.t
+  val all_from_body : Cohttp_lwt.Body.t -> t list Lwt.t
   val fetch_active : api_token:string -> t list Lwt.t
 end
 
-type message = { id : string; name : string; text : string; user_id : string }
+module Message : sig
+  type t = { id : string; name : string; text: string; user_id: string }
 
-val fetch_messages_for_group : api_token:string -> group_id:string -> chunk_size:int -> message list Lwt.t
-val make_messages_url : api_token:string -> group_id:string -> limit:int -> before_id:string Option.t -> string
+  val from_json : Yojson.Basic.t -> t
+  val all_from_body : Cohttp_lwt.Body.t -> t list Lwt.t
+  val fetch_all_for_group : api_token:string -> group_id:string -> chunk_size:int -> t list Lwt.t
+end
