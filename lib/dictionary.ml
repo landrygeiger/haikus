eefe32430832abd7e60ff8cb16e31c9d436b3669
Landry Geiger <69441011+landrygeiger@users.noreply.github.com>
2024-08-22 10:57:27 -0500

    Move dictionary util to util module

--- lib/dictionary.ml
+++ lib/dictionary.ml
@@ -1,18 +1,20 @@
-let is_alpha = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
+module Util = struct
+  let is_alpha = function 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
 
-let explode s =
-  let rec exp i l = if i < 0 then l else exp (i - 1) (s.[i] :: l) in
-  exp (String.length s - 1) []
+  let explode s =
+    let rec exp i l = if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+    exp (String.length s - 1) []
 
-let rec unexplode = function
-  | c :: cs -> String.make 1 c ^ unexplode cs
-  | [] -> ""
+  let rec unexplode = function
+    | c :: cs -> String.make 1 c ^ unexplode cs
+    | [] -> ""
 
-let filter_chars pred str = str |> explode |> List.filter pred |> unexplode
-let filter_non_alphanumerics = filter_chars is_alpha
+  let filter_chars pred str = str |> explode |> List.filter pred |> unexplode
+  let filter_non_alphanumerics = filter_chars is_alpha
 
-let words_from_text str =
-  str
-  |> Str.split (Str.regexp "[ \n\r\x0c\t]+")
-  |> List.map filter_non_alphanumerics
-  |> List.map String.lowercase_ascii
+  let words_from_text str =
+    str
+    |> Str.split (Str.regexp "[ \n\r\x0c\t]+")
+    |> List.map filter_non_alphanumerics
+    |> List.map String.lowercase_ascii
+end
